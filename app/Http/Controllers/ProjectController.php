cff34ee25b1a1ae61c9d7671786a65b3b8b6b0a9
pedroquack <pedrovolpattocosta@gmail.com>
2024-09-07 14:25:05 -0300

    add: Metodos Store e Update dos projetos

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\Roles;
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('projects.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'start_date' => 'date|required',
+            'end_date' => 'date|after:start_date|required',
+            'user_id' => 'required|exists:users,id',
+        ]);
+
+        $user = User::find($request->user_id);
+        if($user->role !== Roles::CLI->value){
+            return redirect()->back()->with('error', 'Somente clientes podem ser associados a projetos!');
+        }
+
+        $project = new Project();
+        $project->title = $request->title;
+        $project->description = $request->description;
+        $project->start_date = $request->start_date;
+        $project->end_date = $request->end_date;
+        $project->user_id = $request->user_id;
+        $project->save();
+
+        return redirect()->route('projects.index')->with('success','Projeto criado com sucesso!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Project $project)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'start_date' => 'date|required',
+            'end_date' => 'date|after:start_date|required',
+            'user_id' => 'required|exists:users,id',
+        ]);
+
+        $user = User::find($request->user_id);
+        if($user->role !== Roles::CLI->value){
+            return redirect()->back()->with('error', 'Somente clientes podem ser associados a projetos!');
+        }
+
+        $project->title = $request->title;
+        $project->description = $request->description;
+        $project->start_date = $request->start_date;
+        $project->end_date = $request->end_date;
+        $project->user_id = $request->user_id;
+        $project->save();
+
+        return redirect()->route('projects.index')->with('success','Projeto editado com sucesso!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Project $project)
+    {
+        if(!isset($project)){
+            return redirect()->back()->with('error','Esse projeto não existe');
+        }
+        return redirect()->route('projects.index')->with('success','Projeto excluído com sucesso!');
+    }
+}
