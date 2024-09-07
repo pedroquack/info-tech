990832d5b5eb1b9787f2e159d22d0fa54ead0ef7
pedroquack <pedrovolpattocosta@gmail.com>
2024-09-07 12:04:55 -0300

    add: Adicionado Migrações e Models dos Projetos e Tarefas

---
+++ app/Models/Task.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'user_id',
+    ];
+
+    use HasFactory;
+
+    public function responsible() : BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+
+    public function project() : BelongsTo{
+        return $this->belongsTo(Project::class);
+    }
+}
